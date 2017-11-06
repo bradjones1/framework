6586fa68771d1562b230c6389b21093cb89447ea
James Finstrom <jfinstrom@sangoma.com>
2017-11-06 15:11:39 -0700

    FREEPBX-12294 Add SSL Redirect
    This adds an SSL Redirect to push from http to https
    This requires an update to sysadmin
    
    This sets a 301 redirect which is cached by browsers. You may wish to
    use incogneto mode for testing.

--- amp_conf/htdocs/index.php
+++ amp_conf/htdocs/index.php
@@ -2,6 +2,14 @@
 //	License for all code of this FreePBX module can be found in the license file inside the module directory
 //	Copyright 2013 Schmooze Com Inc.
 //
+//SSL Redirect
+if(isset($_SERVER['HTTP_X_FREEPBXSSL']) && !isset($_SERVER['HTTPS'])){
+	$redirect = 'https://'.$_SERVER['HTTP_HOST'].$_SERVER['REQUEST_URI'];
+	header("HTTP/1.1 301 Moved Permanently");
+	header('Location: ' . $redirect);
+	exit;
+}
+
 if (file_exists(dirname(__FILE__) . '/index_custom.php')) {
 	include_once(dirname(__FILE__) . '/index_custom.php');
 } else {
